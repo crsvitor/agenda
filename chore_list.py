236ac1348e27d4222ac0ebfe4c54e0c110096450
Vitor <dev.castrors@gmail.com>
2024-06-08 22:06:54 -0300

    feat: add user chore options

---
+++ chore_list.py
@@ -0,0 +1,49 @@
+chores = []
+
+def add_chore():
+    return
+
+def list_chores():
+    return
+
+def update_chore():
+    return
+
+def complete_chore():
+    return
+
+def delete_completed_chores():
+    return
+
+def end_session():
+    return
+
+def start_session():
+    return
+
+user_options = [
+    {
+        "text": "Add chore",
+        "function": add_chore, 
+    },
+    {
+        "text": "List chores",
+        "function": list_chores,
+    },
+    {
+        "text": "Update chore",
+        "function": update_chore,
+    },
+    {
+        "text": "Complete chore",
+        "function": complete_chore,
+    },
+    {
+        "text": "Delete completed chores",
+        "function": delete_completed_chores,
+    },
+    {
+        "text": "End session",
+        "function": end_session,
+    }
+]
